add646fcccb4e473f0fc5cc2b162583f6f30e812
Eduardo <lalin03032000@gmail.com>
2020-04-14 15:57:01 -0500

    Updates

---
+++ precache-manifest.4c44dad1e637a65afc1b14ba2cf36c60.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8075bd142449ad997719a8291116af",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/index.html"
+  },
+  {
+    "revision": "0f335a1db0e3aa5a409d",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/static/css/2.85f22f3a.chunk.css"
+  },
+  {
+    "revision": "2eecbf1e2eac78bc39f0",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/static/css/main.ed978b40.chunk.css"
+  },
+  {
+    "revision": "0f335a1db0e3aa5a409d",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/static/js/2.08565a3f.chunk.js"
+  },
+  {
+    "revision": "0261e2dee4a7e8969069abb914658242",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/static/js/2.08565a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eecbf1e2eac78bc39f0",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/static/js/main.2585f0bf.chunk.js"
+  },
+  {
+    "revision": "ebbc8ab4e1b61f17e403",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/static/js/runtime-main.6777ed48.js"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/git@github.com:JoseEduardoRomero/ProyectoReact.git/static/media/stars.0abcdfcd.svg"
+  }
+]);
